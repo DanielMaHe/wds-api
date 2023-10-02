32f3a8346c9bb4376e6388e00348605dcd781a0e
DanielMaHe <martinezh.dani@gmail.com>
2023-10-02 16:38:32 -0600

    add validates to Worker

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,4 +1,8 @@
 class Worker < ApplicationRecord
-    has_secure_token
-    has_secure_password
+  has_secure_token
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :mail, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 end
