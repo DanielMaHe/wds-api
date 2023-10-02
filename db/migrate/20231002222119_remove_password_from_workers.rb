ba68aada006bff0bb8e022136c167754e863d927
DanielMaHe <martinezh.dani@gmail.com>
2023-10-02 16:22:15 -0600

    remove column password from Worker

---
+++ db/migrate/20231002222119_remove_password_from_workers.rb
@@ -0,0 +1,5 @@
+class RemovePasswordFromWorkers < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :workers, :password, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_02_221736) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_02_222119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,7 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_02_221736) do
 
   create_table "workers", force: :cascade do |t|
     t.string "username"
-    t.string "password"
     t.boolean "isAdmin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
