955361fcfd291b9b302c23b0a024f8a63e553eb3
DanielMaHe <martinezh.dani@gmail.com>
2023-10-02 14:47:33 -0600

    add Proyect entity with scaffold

---
+++ app/controllers/proyects_controller.rb
@@ -0,0 +1,51 @@
+class ProyectsController < ApplicationController
+  before_action :set_proyect, only: %i[ show update destroy ]
+
+  # GET /proyects
+  def index
+    @proyects = Proyect.all
+
+    render json: @proyects
+  end
+
+  # GET /proyects/1
+  def show
+    render json: @proyect
+  end
+
+  # POST /proyects
+  def create
+    @proyect = Proyect.new(proyect_params)
+
+    if @proyect.save
+      render json: @proyect, status: :created, location: @proyect
+    else
+      render json: @proyect.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /proyects/1
+  def update
+    if @proyect.update(proyect_params)
+      render json: @proyect
+    else
+      render json: @proyect.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /proyects/1
+  def destroy
+    @proyect.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_proyect
+      @proyect = Proyect.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def proyect_params
+      params.require(:proyect).permit(:company, :status, :comment)
+    end
+end
---
+++ test/controllers/proyects_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class ProyectsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @proyect = proyects(:one)
+  end
+
+  test "should get index" do
+    get proyects_url, as: :json
+    assert_response :success
+  end
+
+  test "should create proyect" do
+    assert_difference("Proyect.count") do
+      post proyects_url, params: { proyect: { comment: @proyect.comment, company: @proyect.company, status: @proyect.status } }, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "should show proyect" do
+    get proyect_url(@proyect), as: :json
+    assert_response :success
+  end
+
+  test "should update proyect" do
+    patch proyect_url(@proyect), params: { proyect: { comment: @proyect.comment, company: @proyect.company, status: @proyect.status } }, as: :json
+    assert_response :success
+  end
+
+  test "should destroy proyect" do
+    assert_difference("Proyect.count", -1) do
+      delete proyect_url(@proyect), as: :json
+    end
+
+    assert_response :no_content
+  end
+end
