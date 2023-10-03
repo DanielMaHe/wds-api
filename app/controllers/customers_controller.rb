4f6531af17c7a7c20353393085fed8e436b13d6c
DanielMaHe <martinezh.dani@gmail.com>
2023-10-03 10:32:42 -0600

    params Requiere to Customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class CustomersController < ApplicationController
-  before_action :set_customer, only: %i[ show update destroy ]
+  before_action :set_customer, only: %i[show update destroy]
 
   # GET /customers
   def index
@@ -24,28 +26,34 @@ class CustomersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /customers/1
-  def update
-    if @customer.update(customer_params)
-      render json: @customer
-    else
-      render json: @customer.errors, status: :unprocessable_entity
-    end
-  end
+  # # PATCH/PUT /customers/1
+  # def update
+  #   if @customer.update(customer_params)
+  #     render json: @customer
+  #   else
+  #     render json: @customer.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # DELETE /customers/1
-  def destroy
-    @customer.destroy
-  end
+  # # DELETE /customers/1
+  # def destroy
+  #   @customer.destroy
+  # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_customer
-      @customer = Customer.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def customer_params
-      params.require(:customer).permit(:name, :email, :telephone, :company, :comment)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def customer_params
+    # params.require(:customer).permit(:name, :email, :telephone, :company, :comment)
+    params.require(
+      :customer
+    ).permit(:name, :email).merge(
+      params[:customer].slice(:telephone, :company, :comment)
+    )
+  end
 end
