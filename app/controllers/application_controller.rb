e6b2a448d7a6c95338918684ee55068b41b36ae5
DanielMaHe <martinezh.dani@gmail.com>
2023-10-04 22:54:00 -0600

    feat: authorize methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,33 @@
 # frozen_string_literal: true
 
+# some doc
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  include Pundit::Authorization
+  before_action :authorize
+
+  # before_action :authenticate_worker!
+  # before_action :require_login
+
+  def current_user
+    authenticate_token
+  end
+
+  def authorize
+    return if authenticate_token
+
+    respond_unauthorized("Access Desnied")
+  end
+
+  def respond_unauthorized(message)
+    render json: {messages: message}, status: :unauthorized
+  end
+
+  private
+
+  def authenticate_token
+    authenticate_with_http_token do |token, _options|
+      Worker.find_by(token: token)
+    end
+  end
 end
