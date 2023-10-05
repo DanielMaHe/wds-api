eec7b8e35eb5305e37cab1d051516467f4c295fb
DanielMaHe <martinezh.dani@gmail.com>
2023-10-04 22:51:36 -0600

    add worker policy

--- app/policies/worker_policy.rb
+++ app/policies/worker_policy.rb
@@ -1,11 +1,16 @@
 class WorkerPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
-    def manage?
-      user.isAdmin?
+    def resolve
+      scope.all
     end
+
+    def manage?
+      user.admin?
+    end
+  end
+
+  def index
+    manage?
   end
 end
