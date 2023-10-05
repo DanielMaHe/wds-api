e77c8ca1b31d542768d4375d1e6af3e77bb294d9
DanielMaHe <martinezh.dani@gmail.com>
2023-10-04 22:52:32 -0600

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  # def new; end
+
+  def create
+    # render json: { messages: "this create"}
+    worker = Worker.find_by(username: params[:session][:username])
+
+    if worker&.authenticate(params[:session][:password])
+      # Iniciar sesión exitosamente.
+      # session[:worker_id] = worker.id
+      worker.regenerate_token
+      render json: { token: worker.token}
+
+    else
+      # Error de inicio de sesión.
+      respond_unauthorized('Incorrect email o password')
+    end
+  end
+
+  def destroy
+    # Cerrar sesión.
+    # session[:worker_id] = nil
+    current_user.invalid_token
+    head :ok
+    # redirect_to root_path, notice: "Cierre de sesión exitoso"
+  end
+end
